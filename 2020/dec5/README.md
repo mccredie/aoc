0fc3fb449f024e0e36fe923bb3569a5784e8e1f8
Matt McCredie <matt.mccredie@crowdstreet.com>
2020-12-04 22:52:43 -0800

    Day 5 complete in clojure.

---
+++ 2020/dec5/README.md
@@ -0,0 +1,9 @@
+To run this solution you must [install clojure](https://clojure.org/guides/getting_started)
+
+Then on the command line:
+  `cat input.txt | clj -X day5/ex1`
+  `cat input.txt | clj -X day5/ex2`
+
+
+
+
---
+++ 2020/dec5/src/day5.clj
@@ -0,0 +1,59 @@
+(ns day5)
+
+(defn read-input []
+  (line-seq (java.io.BufferedReader. *in*)))
+
+(defn row-part [boarding-pass]
+  (subs boarding-pass 0 7))
+
+(defn row-value [row-part]
+  (reduce
+    (fn [a b] (+ (* 2 a) b))
+    0
+    (map (fn [c] (if (= c \F) 0 1)) row-part)))
+
+(defn col-part [boarding-pass]
+  (subs boarding-pass 7 10))
+
+(defn col-value [col-part]
+  (reduce
+    (fn [a b] (+ (* 2 a) b))
+    0
+    (map (fn [c] (if (= c \L) 0 1)) col-part)))
+
+(defn col-id [boarding-pass]
+    (->>
+      boarding-pass
+      (col-part)
+      (col-value)))
+
+(defn row-id [boarding-pass]
+    (->>
+      boarding-pass
+      (row-part)
+      (row-value)))
+
+(defn seat-id [boarding-pass]
+  (+
+    (* 8 (row-id boarding-pass))
+    (col-id  boarding-pass)))
+
+(defn ex1 [opts]
+  (println (reduce max (map seat-id (read-input)))))
+
+(defn empty-seats [seats]
+  (reduce disj (set (range 1023)) seats))
+
+(defn find-loner [seats]
+  (->>
+    (empty-seats seats)
+    (filter (fn [seat] (and (contains? seats (inc seat)) (contains? seats (dec seat)))))
+    (first)
+  ))
+
+(defn ex2 [opts]
+  (println (find-loner (set (map seat-id (read-input))))))
+
+
+
+
