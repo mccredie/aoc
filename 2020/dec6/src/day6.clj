304a99d268e91582818730caf09b67cef6737745
Matt McCredie <matt.mccredie@crowdstreet.com>
2020-12-06 00:06:48 -0800

    Completed day 6

---
+++ 2020/dec6/src/day6.clj
@@ -0,0 +1,38 @@
+(ns day6
+  (:require [clojure.set :as cset :refer [intersection]]))
+
+(declare read-input)
+
+(defn get-groups-any [lines]
+  (let [[groups lastgroup]
+        (reduce
+          (fn
+            [[groups current] row]
+            (if (empty? row)
+              [(conj groups current) #{}]
+              [groups (reduce conj current row)]))
+          [[] #{}]
+          lines)]
+    (conj groups lastgroup)))
+
+(defn ex1 [opts]
+  (println (reduce + (map count (get-groups-any (read-input))))))
+
+(defn get-groups-all [lines]
+  (let [[groups lastgroup]
+        (reduce
+          (fn
+            [[groups current] row]
+            (if (empty? row)
+              [(conj groups current) (set "abcdefghijklmnopqrstuvwxyz")]
+              [groups (cset/intersection current (set row))]))
+          [[] (set "abcdefghijklmnopqrstuvwxyz")]
+          lines)]
+    (conj groups lastgroup)))
+
+(defn ex2 [opts]
+  (println (reduce + (map count (get-groups-all (read-input))))))
+
+(defn read-input []
+  (line-seq (java.io.BufferedReader. *in*)))
+
